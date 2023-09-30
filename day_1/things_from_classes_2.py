729002082b6be28ebf72a642c8a1f19a7085959c
enzslv4 <128191863+Enzslv4@users.noreply.github.com>
2023-09-30 13:04:38 -0300

    Update things_from_classes_2.py

--- day_1/things_from_classes_2.py
+++ day_1/things_from_classes_2.py
@@ -6,7 +6,7 @@ while True:
     name = input('Your name: ')
     try:
         type(int(name)) == True # i know that there is a better way to do this
-        # but i just forget how to kkkk
+        # but i just forgot how to kkkk
         print('Invalid argument')
         os.system('cls')
     except ValueError:
