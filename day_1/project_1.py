f9ae289bb606bd75fec94fbcccd0e2198975438d
Enzo Silva <enzosilva142002@gmail.com>
2023-09-30 11:59:44 -0300

    Updating

---
+++ day_1/project_1.py
@@ -0,0 +1,7 @@
+# first project at the course: Band name
+
+city_name = input('What is the name of the city that u grew up? ')
+pet_name = input('What is your pet name? ')
+
+print()
+print('Your rock band name is', city_name, pet_name)
---
+++ day_1/project_2.py
@@ -0,0 +1,3 @@
+# testing print options
+
+print('Hello world\n' * 2 + 'Hello world')
