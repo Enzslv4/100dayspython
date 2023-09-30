a7caa97f4f7da3cda4db2bf4f40560de94665839
Enzo Silva <enzosilva142002@gmail.com>
2023-09-30 13:02:30 -0300

    day 1 finished?

---
+++ day_1/things_from_classes_2.py
@@ -0,0 +1,14 @@
+# counting strings
+
+import os
+
+while True:
+    name = input('Your name: ')
+    try:
+        type(int(name)) == True # i know that there is a better way to do this
+        # but i just forget how to kkkk
+        print('Invalid argument')
+        os.system('cls')
+    except ValueError:
+        print(f'Your name has {len(name)} letters.')
+        break
