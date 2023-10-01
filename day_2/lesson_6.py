33b70adf69c00883d2a8ce821f7ce2329fbab7e2
Enzo Silva <enzosilva142002@gmail.com>
2023-10-01 20:33:31 -0300

    lesson 6 added

---
+++ day_2/lesson_6.py
@@ -0,0 +1,8 @@
+#   Calculating BMI
+
+print('BMI calculator.')
+
+weight = float(input('Digit your weight: '))
+height = float(input('Digit your height: '))
+
+print('Your BMI is', weight / height ** 2)
