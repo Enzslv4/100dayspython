4a268dd7be60ee150a9c24352cd761bde5b1fd7c
Enzo Silva <enzosilva142002@gmail.com>
2023-10-12 10:27:46 -0300

    last updates

--- day_4/project.py
+++ day_4/project.py
@@ -7,8 +7,6 @@ import math
 possibles_answers = [0, 1, 2]
 answers_names = ['Rock ✊', 'Paper ✋', 'Scissors ✌️']
 
-
-
 print('Welcome to the game paper, scissors and rock.')
 print()
 
@@ -16,16 +14,16 @@ score = 0
 
 while True:
     print(f'The score is {score}/5 winnings.')
-    print('Please chose one of them:')
-    x = int(input('0 for Rock, 1 for Paper and 2 for Scissors: '))
+    print('Please chose one of them')
+    x = int(input('0 for Rock, 1 for Paper and 2 for Scissors:\n'))
     y = random.randint(0,2)
-    module_subtraction = int(math.fabs(x - y))
+    subtraction_module = int(math.fabs(x - y))
     if x in possibles_answers:
         print('You choose: ', answers_names[x])
         print('The computer answer is:', answers_names[y])
         if x == y:
             print('You Tied.')
-        elif module_subtraction == 1:
+        elif subtraction_module == 1:
             if x > y:
                 score += 1
                 print('You Win! 👏👏👏')
@@ -43,9 +41,9 @@ while True:
         print('Wrong argument.')
     if score < 0:
         score = 0
-        print('Game over!')
+        print('\nGame over!\n')
 
-    reset = input('Do you wanna to try again? ')
+    reset = input('Do you wanna try again?\n')
     if reset == 'y':
         if score == 5:
             print('Game Over, You Win! 👏👏👏')
