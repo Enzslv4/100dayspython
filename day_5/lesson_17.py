4a118fd93c9e23e79a1191dee15cef4da05eb379
Enzo Silva <enzosilva142002@gmail.com>
2023-10-12 10:28:39 -0300

    last updates

---
+++ day_5/lesson_17.py
@@ -0,0 +1,10 @@
+# major number
+
+student_scores = [78, 65, 89, 86, 55, 91, 64, 89]
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+
+print(highest_score)
+    
