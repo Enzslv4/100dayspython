5648628f948f2e38448eb4da297cc5f637b119c5
Enzo Silva <enzosilva142002@gmail.com>
2023-10-13 23:09:10 -0300

    day 5 complete, and day 6

--- day_5/project.py
+++ day_5/project.py
@@ -10,50 +10,31 @@ numb_simbols = int(input('How many simbols do you want?\n'))
 numb_numbers = int(input('How many numbers do you want?\n'))
 total_chars = numb_letters + numb_numbers + numb_simbols
 
-a = 0
-b = 0
-c = 0
-state = True
 letters = string.ascii_letters
 simbols = string.punctuation
-password = ''
+normal_password = []
+final_password = ''
 
-for d in range(0, total_chars):
-    
-    if state:
-        ...
-    else:
-        d -= 1
-        state = True
-    
-    rand_char = random.choice(letters)
-    rand_simb = random.choice(simbols)
-    rand_numb = random.randint(0,9)
-    possible_char = [rand_char, rand_simb, rand_numb]
-    i = random.randint(0,2)
+for char in range(0, numb_letters):
+    normal_password += random.choice(letters)
 
-    if i == 0:
-        if a < numb_letters:
-            password += str(possible_char[i])
-            a += 1
-        else:
-            state = False
-            continue
-    elif i == 1:
-        if b < numb_simbols:
-            password += str(possible_char[i])
-            b += 1
-        else:
-            state = False
-            continue
-    elif i == 2:
-        if c < numb_numbers:
-            password += str(possible_char[i])
-            c += 1
-        else:
-            state = False
-            continue
-    
+for char in range(0, numb_numbers):
+    normal_password += str(random.randint(0,9))
 
-print(password)
-    
+
+for char in range(0, numb_simbols):
+    normal_password += random.choice(simbols)
+
+i = 0
+while True:
+    i = random.randint(0, len(normal_password) - 1)
+    lenth = len(normal_password)
+    final_password += normal_password[i]
+    normal_password.pop(i)
+    lenth -= 1
+    if lenth == 0:
+        break
+
+print("You password is", final_password)
+
+# random.shuffle(x) - this is a way more simple way to do the same as i did above
