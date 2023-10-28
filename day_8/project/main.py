833bd57fef85cb99b5177302792ec456440aa23a
Enzo Silva <enzosilva142002@gmail.com>
2023-10-28 12:26:17 -0300

    more and more

--- day_8/project/main.py
+++ day_8/project/main.py
@@ -1,7 +1,7 @@
 # cesar cipher
 
 from resources import letters
-from d_encrypter import encrypter, decrypter
+from day_8.project.functions import encrypter, decrypter, int_check
 
 decision = input('Type "e" to encrypt, or "d" to decrypt: ')
 final_result = []
@@ -10,10 +10,8 @@ if decision == 'e':
     to_encrypt = input('Type your message: ')
 
     if to_encrypt.isalpha():
-        try:
-            shift_num = int(input('Type the shift number: '))
-        except ValueError:
-            print('Wrong argument.')
+        shift_num = input('Type the shift number: ')
+        int_check(shift_num)
         
         encrypter(to_encrypt, letters, shift_num, final_result)
 
