ace622fbfc8ca7c075ca77c4ee68efbef535d622
Enzo Silva <enzosilva142002@gmail.com>
2023-11-19 19:59:41 -0300

    my result of the random path challenge

--- day_18/random_path.py
+++ day_18/random_path.py
@@ -4,33 +4,35 @@ from turtle import Turtle, Screen
 import random
 
 timmy = Turtle()
-timmy.shape('turtle')
+timmy.pensize(10)
 
 screen = Screen()
 screen.colormode(255)
 
 def movement():
 
-    n = 3
     colors = {
         'r': 0,
         'g': 0,
         'b': 0
     }
 
-    while n <= 10:
+    while True:
 
         for key in colors.keys():
             color = random.randint(0, 255)
             colors[key] = color
 
         timmy.pencolor(colors['r'], colors['g'], colors['b'])
-        angle = 360 / n
 
-        for _ in range(n):
-            timmy.forward(100)
-            timmy.right(angle)
-        n += 1
+        side = random.choice(['l', 'r'])
+
+        if side == 'l':
+            timmy.forward(50)
+            timmy.left(90)
+        else:
+            timmy.forward(50)
+            timmy.right(90)
 
 movement()
 
