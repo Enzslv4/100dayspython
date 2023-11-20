bea300b7e335df0b6fcc17568ff5670a8f7384bb
Enzo Silva <enzosilva142002@gmail.com>
2023-11-20 16:37:59 -0300

    att

--- day_18/project.py
+++ day_18/project.py
@@ -7,11 +7,16 @@ timmy = Turtle()
 timmy.pensize(10)
 timmy.speed(8)
 timmy.width(5)
+timmy.penup()
 
 screen = Screen()
 screen.colormode(255)
 
 def movement():
+
+    timmy.setheading(210)
+    timmy.forward(300)
+    timmy.setheading(0)
 
     colors = {
         'r': 0,
@@ -21,32 +26,28 @@ def movement():
 
     turn_state = True
 
-    for _ in range(20):
 
-        timmy.pendown()
+    for _ in range(10):
 
-        for _ in range(20):
+        for _ in range(10):
 
             for key in colors.keys():
                 color = random.randint(0, 255)
                 colors[key] = color
 
-            timmy.color(colors['r'], colors['g'], colors['b'])
-
-            timmy.dot(10)
-            timmy.penup()
-            timmy.forward(20)
+            timmy.dot(20, (colors['r'], colors['g'], colors['b']))
+            timmy.forward(50)
         
         if turn_state == True:
             timmy.left(90)
-            timmy.dot(10)
-            timmy.forward(20)
+            timmy.dot(20)
+            timmy.forward(50)
             timmy.left(90)
             turn_state = False
         else:
             timmy.right(90)
-            timmy.dot(10)
-            timmy.forward(20)
+            timmy.dot(20)
+            timmy.forward(50)
             timmy.right(90)
             turn_state = True
 
---
+++ day_18/sinusoid.py
@@ -0,0 +1,55 @@
+# trying to make a sinusoid on turtle
+
+from turtle import Turtle, Screen
+import random
+import math
+
+timmy = Turtle()
+
+screen = Screen()
+screen.colormode(255)
+
+pi = math.pi
+
+# def sen(angle):
+#     result = (pi * angle) / 180
+#     # if result > 1:
+#     #     divisor = math.floor(result / 1)
+#     #     result = result - 1 * divisor
+#     # elif result < -1:
+#     #     result *= -1
+#     #     divisor = math.floor(result / 1)
+#     #     result = (-1) * (result - 1 * divisor)
+#     return result
+
+
+def movement():
+    ang = 30 # the initial angle
+    a = 100 # amplitude
+    f = 50 # frequency
+    t = 0 # time
+    x = 0 # x value on grafic
+    c = 300000000 # light speed
+    wave_length = c / f
+    omega = 2 * pi
+    k = omega / wave_length
+    
+    while True:
+        t += 1
+        inside_sen = (k*x) - (omega * t) + ang # everything that goes inside de sen func
+        if inside_sen > 360:
+            divisor = math.floor(inside_sen / 360)
+            inside_sen = inside_sen - 360 * divisor
+        elif inside_sen < 0:
+            inside_sen *= -1
+            divisor = math.floor(inside_sen / 360)
+            inside_sen = (-1) * (inside_sen - 360 * divisor)
+        rad = math.radians(inside_sen)
+        seno = math.sin(rad)
+        y = a * seno
+        timmy.setpos(x, y)
+        x += 1
+
+movement()
+
+screen.mainloop()
