aa9ebd589febdfdfa54a23f0b082c5e219f19bb2
Enzo Silva <enzosilva142002@gmail.com>
2023-12-04 17:06:02 -0300

    day 27 done

--- day_27/project/main.py
+++ day_27/project/main.py
@@ -5,27 +5,36 @@ from tkinter import *
 from time import sleep
 
 window = Tk()
-window.title('Miles converter')
-window.minsize(300, 100)
+window.title('Miles to Km converter')
+window.config(padx=20, pady=20)
 
-my_label = Label(text='Convert Miles to Km',font=('Arial', 10, 'bold'),) #something that will be shown on screen
-my_label.pack()
-result_label = Label()
+label_miles = Label(text='Miles') #something that will be shown on screen
+label_miles.grid(column=2, row=0)
+
+
+result_label = Label(text='0')
+result_label.grid(column=1,row=1)
+
+
+label_equal_to = Label(text='is equal to')
+label_equal_to.grid(column=0, row=1)
+
+km_label = Label(text='Km')
+km_label.grid(column=2, row=1)
 
 def clicked():
     num_input = text_box.get()
     result = int(num_input) * 1.61
-    result_label.config(text=f'The result is {result:.2f} kilometers')
-    result_label.pack()
+    result_label.config(text=f'{result:.2f}')
+    result_label.grid(column=1, row=1)
 
 
 text_box = Entry(width=10)
-text_box.pack()
+text_box.grid(column=1, row=0)
 
 
-
-button = Button(text='convert', command=clicked)
-button.pack()
+calculate_buttom = Button(text='Calculate', command=clicked)
+calculate_buttom.grid(column=1, row=2)
 
 
 window.mainloop()
