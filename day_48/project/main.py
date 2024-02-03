0b045450a71393d4ce8e7800841108b1dfda6ff6
Enzo Silva <enzosilva142002@gmail.com>
2024-02-03 10:23:52 -0300

    daily update

--- day_48/project/main.py
+++ day_48/project/main.py
@@ -1,2 +1,8 @@
-import selenium
+from selenium import webdriver
 
+amazon_url = 'https://www.amazon.com'
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option('detach', True)
+
+driver = webdriver.Edge(options=chrome_options)
+driver.get(amazon_url)
