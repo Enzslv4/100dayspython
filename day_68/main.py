457efc099a565ce4cb31c59975dabf4ea4d314ed
Enzo Silva <enzosilva142002@gmail.com>
2024-07-11 22:55:13 -0300

    day 68 done

--- day_68/main.py
+++ day_68/main.py
@@ -39,12 +39,19 @@ with app.app_context():
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    # Passing True or False if the user is authenticated. 
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        email = request.form.get('email')
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
+        if user:
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
         hash_and_salted_password = generate_password_hash(
             request.form.get('password'),
             method='pbkdf2:sha256',
@@ -52,20 +59,15 @@ def register():
         )
         new_user = User(
             email=request.form.get('email'),
-            name=request.form.get('name'),
             password=hash_and_salted_password,
+            name=request.form.get('name'),
         )
-
         db.session.add(new_user)
         db.session.commit()
-
-        # Log in and authenticate user after adding details to database.
         login_user(new_user)
-
-        # Can redirect() and get name from the current_user
         return redirect(url_for("secrets"))
-
-    return render_template("register.html")
+    # Passing True or False if the user is authenticated. 
+    return render_template("register.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -73,26 +75,26 @@ def login():
     if request.method == "POST":
         email = request.form.get('email')
         password = request.form.get('password')
-
-        # Find user by email entered.
         result = db.session.execute(db.select(User).where(User.email == email))
         user = result.scalar()
-
-        # Check stored password hash against entered password hashed.
-        if check_password_hash(user.password, password):
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for('login'))
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('login'))
+        else:
             login_user(user)
             return redirect(url_for('secrets'))
+    # Passing True or False if the user is authenticated. 
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
-    return render_template("login.html")
 
-
-# Only logged-in users can access the route
 @app.route('/secrets')
 @login_required
 def secrets():
     print(current_user.name)
-    # Passing the name from the current_user
-    return render_template("secrets.html", name=current_user.name)
+    return render_template("secrets.html", name=current_user.name, logged_in=True)
 
 
 @app.route('/logout')
