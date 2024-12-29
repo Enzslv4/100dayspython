5f488a3a0b41e252c4fdded4a1bedc25df3a7970
unknown <enzosilva142002@gmail.com>
2024-12-29 19:05:18 -0300

    test

--- day_62/main.py
+++ day_62/main.py
@@ -31,7 +31,7 @@ class CafeForm(FlaskForm):
 # Exercise:
 # add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
 # make coffee/wifi/power a select element with choice of 0 to 5.
-#e.g. You could use emojis ☕️/💪/✘/🔌
+#e.g. You could use emojis ☕️/💪/✘/🔌 
 # make all fields required except submit
 # use a validator to check that the URL field has a URL entered.
 # ---------------------------------------------------------------------------
