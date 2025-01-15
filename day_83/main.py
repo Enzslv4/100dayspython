a3af46505e150b1af17bb72bfe3e0d0bdabb2cd0
unknown <enzosilva142002@gmail.com>
2025-01-15 09:24:56 -0300

    day 83 complete, later i'll do some upgrades

--- day_83/main.py
+++ day_83/main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/resume')
+def resume():
+    return render_template("resume.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
